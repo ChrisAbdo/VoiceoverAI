15f97f14141f95d9388825ce8edd5716b4d028c4
Chris Abdo <chris.j.abdo@gmail.com>
2023-11-14 18:50:12 -0500

    api key

--- lib/openai-provider.ts
+++ lib/openai-provider.ts
@@ -1,6 +1,5 @@
 import OpenAI from "openai";
 
 export const openai = new OpenAI({
-  apiKey: "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",
-  dangerouslyAllowBrowser: true,
+  apiKey: process.env.OPENAI_API_KEY,
 });
